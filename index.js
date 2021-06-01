781170c1a60b8506ba25029d0225146ba6163979
Alataq <72706324+Alataq@users.noreply.github.com>
2021-06-01 16:36:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const client = new Discord.Client
 var login = function(token){
     client.login(token)
-    console.log("Bot login at " + client.user.tag)
+    console.log("Bot login")
 }
 var newCommand = function(prefix, trigger, reply){
     client.on("message", message => {
